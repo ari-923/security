f0d38d4bf5389ce769a2482a6f3cd4ed4d06200d
Ari <113228939+ari-923@users.noreply.github.com>
2026-09-08 08:32:14 -0700

    Update index.js

--- content/index.js
+++ content/index.js
@@ -37,8 +37,6 @@ import lesson_08_03 from "./lessons/08-03.json";
 import lesson_08_04 from "./lessons/08-04.json";
 import lesson_09_01 from "./lessons/09-01.json";
 import lesson_09_02 from "./lessons/09-02.json";
-import lesson_09_03 from "./lessons/09-03.json";
-import lesson_09_04 from "./lessons/09-04.json";
 import lesson_10_01 from "./lessons/10-01.json";
 import lesson_10_02 from "./lessons/10-02.json";
 import lesson_10_03 from "./lessons/10-03.json";
@@ -76,7 +74,7 @@ import lesson_17_01 from "./lessons/17-01.json";
 import lesson_17_02 from "./lessons/17-02.json";
 import lesson_17_03 from "./lessons/17-03.json";
 
-export const lessons = [lesson_01_02, lesson_02_01, lesson_02_02, lesson_02_03, lesson_02_04, lesson_02_05, lesson_02_06, lesson_02_07, lesson_03_01, lesson_03_02, lesson_03_03, lesson_04_01, lesson_04_02, lesson_04_03, lesson_04_04, lesson_04_05, lesson_04_06, lesson_04_07, lesson_04_08, lesson_04_09, lesson_04_10, lesson_05_01, lesson_05_02, lesson_05_03, lesson_06_01, lesson_06_02, lesson_06_03, lesson_06_04, lesson_07_01, lesson_07_02, lesson_07_03, lesson_08_01, lesson_08_02, lesson_08_03, lesson_08_04, lesson_09_01, lesson_09_02, lesson_09_03, lesson_09_04, lesson_10_01, lesson_10_02, lesson_10_03, lesson_10_04, lesson_10_05, lesson_10_06, lesson_10_07, lesson_10_08, lesson_10_09, lesson_10_10, lesson_10_11, lesson_10_12, lesson_10_13, lesson_10_14, lesson_11_01, lesson_11_02, lesson_12_01, lesson_12_02, lesson_12_03, lesson_12_04, lesson_13_01, lesson_13_02, lesson_13_03, lesson_13_04, lesson_13_05, lesson_13_06, lesson_13_07, lesson_15_01, lesson_16_01, lesson_16_02, lesson_16_03, lesson_16_04, lesson_16_05, lesson_17_01, lesson_17_02, lesson_17_03];
+export const lessons = [lesson_01_02, lesson_02_01, lesson_02_02, lesson_02_03, lesson_02_04, lesson_02_05, lesson_02_06, lesson_02_07, lesson_03_01, lesson_03_02, lesson_03_03, lesson_04_01, lesson_04_02, lesson_04_03, lesson_04_04, lesson_04_05, lesson_04_06, lesson_04_07, lesson_04_08, lesson_04_09, lesson_04_10, lesson_05_01, lesson_05_02, lesson_05_03, lesson_06_01, lesson_06_02, lesson_06_03, lesson_06_04, lesson_07_01, lesson_07_02, lesson_07_03, lesson_08_01, lesson_08_02, lesson_08_03, lesson_08_04, lesson_09_01, lesson_09_02, lesson_10_01, lesson_10_02, lesson_10_03, lesson_10_04, lesson_10_05, lesson_10_06, lesson_10_07, lesson_10_08, lesson_10_09, lesson_10_10, lesson_10_11, lesson_10_12, lesson_10_13, lesson_10_14, lesson_11_01, lesson_11_02, lesson_12_01, lesson_12_02, lesson_12_03, lesson_12_04, lesson_13_01, lesson_13_02, lesson_13_03, lesson_13_04, lesson_13_05, lesson_13_06, lesson_13_07, lesson_15_01, lesson_16_01, lesson_16_02, lesson_16_03, lesson_16_04, lesson_16_05, lesson_17_01, lesson_17_02, lesson_17_03];
 export { chapters, scenarioQuestions };
 
 export function getLesson(id) {
